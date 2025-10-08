fa18925d1d1455a234b9242511a795b8b2d05b7d
fasiyam <famimahmedsiyam@gmail.com>
2025-10-08 17:21:11 +0600

    add responsive hero section

---
+++ src/Components/HeroSection/HeroSection.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import heroImg from "../../assets/hero.png"
+import playStoreImg from "../../assets/playStore.png";
+import appStoreImg from "../../assets/appStore.png";
+
+const HeroSection = () => {
+    return (
+        <div className='flex flex-col items-center justify-center mt-10 md:mt-20'>
+            <h1 className='md:text-7xl text-4xl text-center mb-5 md:mb-10'>We Build <br /><span className='bg-gradient-to-r from-[rgba(99,46,227,1)] to-[rgba(159,98,242,1)] bg-clip-text text-transparent'>Productive</span> Apps</h1>
+            <p className='text-xl text-[#627382] text-center max-w-[980px] p-4'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+
+            <div className='mb-10 flex items-center justify-center gap-5 mt-10'>
+                <button className='btn text-xl flex items-center justify-center gap-2.5'>
+                    <img src={playStoreImg} alt="" />
+                    Google Play
+                </button>
+                <button className='btn text-xl flex items-center justify-center gap-2.5'>
+                    <img src={appStoreImg} alt="" />
+                    App Store
+                </button>
+            </div>
+
+            <img src={heroImg} alt="" />
+        </div>
+    );
+};
+
+export default HeroSection;
--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
 import { Outlet } from 'react-router';
+import HeroSection from "../../Components/HeroSection/HeroSection"
 
 const Root = () => {
     return (
         <div className='bg-[#f5f5f5]'>
             <Navbar></Navbar>
+            <HeroSection></HeroSection>
             <Outlet></Outlet>
         </div>
     );
